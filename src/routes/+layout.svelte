e21da349f05bbfcd2aa8b035d6eb5b05b11b1b88
boustifaille <anthonyboisset06@gmail.com>
2023-05-21 21:23:57 +0200

    add header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+    import "../app.css"
+</script>
+
+<header>
+    <div class="logo"></div>
+    <nav>
+        <ul>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/">helloword</a></li>
+            <li><a href="/">helloword</a></li>
+        </ul>
+    </nav>
+</header>
+
+<slot />
+
+<style>
+    header {
+        width: 100%;
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        padding: 0 20px;
+    }
+
+    nav {
+        width: 50%;
+        height: 100%;
+    }
+
+    ul {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
