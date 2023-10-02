10f8558795dd65e1cf62eabfd2a8d503860a3b43
antho <anthonyboisset06@gmail.com>
2023-10-02 20:04:17 +0200

    remove netlify dir

--- .netlify/server/nodes/3.js
+++
@@ -1,9 +0,0 @@
-import * as server from '../entries/pages/_coords_/_page.server.js';
-
-export const index = 3;
-export const component = async () => (await import('../entries/pages/_coords_/_page.svelte.js')).default;
-export { server };
-export const server_id = "src/routes/[coords]/+page.server.js";
-export const imports = ["_app/immutable/nodes/3.86d88ea8.js","_app/immutable/chunks/index.3db2f420.js","_app/immutable/chunks/singletons.2a050726.js"];
-export const stylesheets = ["_app/immutable/assets/3.7efc30c9.css"];
-export const fonts = [];
